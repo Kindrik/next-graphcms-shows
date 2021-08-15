36649665b3d40f6b37a7948a787b33b4d3d6efa2
Kindrik <winterstorm_11@hotmail.com>
2021-08-14 18:53:19 -0600

    added the 404 page, and the switch in the shows api call to divert to it in the event of a non existentpage

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+
+
+
+
+import Layout from '@c/Layout'
+
+export default function Custom404() {
+    return(
+        <Layout title="404 - Page Not Found" maxWidth="800px">
+            <h1>404</h1>
+            <p>Nothing to see here!</p>
+        </Layout>
+    )
+  }
